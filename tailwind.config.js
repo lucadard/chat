2e17963503e8468f68a7e3943149988d236b3fc7
lucadard <lucadardenne@hotmail.com>
2023-04-29 17:24:30 -0300

    extend colors in theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      colors: {
+        primary: '#010409',
+        nav: '#161B22',
+        aside: '#0D1117',
+        links: '#2f81f7',
+        secondary: '#21262D',
+        'secondary-border': '#363B42',
+        'secondary-text': '#7D8590'
       }
     }
   },
