8908f41f3d7d9861f1aaccbfa8f405c9f956fd12
lucadard <lucadardenne@hotmail.com>
2023-05-04 10:22:13 -0300

    change credentials from json to env var

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,12 +1,11 @@
 import { ServiceAccount, initializeApp } from 'firebase-admin/app'
 import { credential } from 'firebase-admin'
 
-import serviceAccount from './firebase-keys.json'
 import { getFirestore } from 'firebase-admin/firestore'
 
 try {
   initializeApp({
-    credential: credential.cert(serviceAccount as ServiceAccount),
+    credential: credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEYS as string) as ServiceAccount),
     databaseURL: 'firebase-adminsdk-shhbt@chat-app-c9ff3.iam.gserviceaccount.com'
   })
 } catch (err) {
