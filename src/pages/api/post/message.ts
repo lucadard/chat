e00402d8e2a392f8262e2e90c8cc152de09675bd
lucadard <lucadardenne@hotmail.com>
2023-05-05 12:55:41 -0300

    implement firebase batch to make multiple writes

--- src/pages/api/post/message.ts
+++ src/pages/api/post/message.ts
@@ -1,28 +1,49 @@
 
 import { firestore } from '@/firebase/admin'
 import { User } from '@/types/types'
+import { runTransaction } from '@firebase/firestore'
+import { FieldValue } from 'firebase-admin/firestore'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { chatId, message, user } = req.body as { chatId: string, message: string, user: User }
-  const added = await firestore.collection(`chats/${chatId}/messages`).add({
+  const { chatId, message, receiver, sender } = req.body as { chatId: string, message: string, receiver: User, sender: User }
+
+  const batch = firestore.batch()
+
+  const messagesRef = firestore.collection(`chats/${chatId}/messages`).doc()
+  batch.set(messagesRef, {
     createdAt: new Date().valueOf(),
     text: message,
     user: {
-      id: user.id,
-      username: user.username
+      id: sender.id,
+      username: sender.username
     }
   })
+  const senderRef = firestore.doc(`/users/${sender.username}`)
+  const receiverRef = firestore.doc(`/users/${receiver.username}`)
+  // updates lastActive field on chat document for both users
+  if (chatId !== 'general') {
+    batch.set(senderRef, {
+      [receiver.id]: {
+        chat_id: chatId,
+        lastActive: new Date().valueOf(),
+        name: receiver.username
+      }
+    }, { merge: true })
+    batch.set(receiverRef, {
+      [sender.id]: {
+        chat_id: chatId,
+        lastActive: new Date().valueOf(),
+        name: sender.username
+      }
+    }, { merge: true })
+  }
 
-  // updates lastActive field on chat document & user
-  firestore.doc(`/chats/${chatId}`)
-    .set({ lastActive: new Date().valueOf() }, { merge: true })
-    .catch(console.error)
-
-  firestore.doc(`/users/${user.username}`)
-    .set({ lastActive: new Date().valueOf() }, { merge: true })
-    .catch(console.error)
-
-  res.status(200).json({ data: added.id })
+  try {
+    await batch.commit()
+    res.status(200).json({ data: messagesRef.id })
+  } catch (err) {
+    res.status(500).json({ error: err })
+  }
 }
