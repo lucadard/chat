297d38be1ee4fc8b201af3dd24b3e50c7047cace
lucadard <lucadardenne@hotmail.com>
2023-05-05 13:02:28 -0300

    only creates a chat and returns id

--- src/pages/api/post/chat.ts
+++ src/pages/api/post/chat.ts
@@ -1,24 +1,12 @@
 import { firestore } from '@/firebase/admin'
-import { User } from '@/types/types'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { users } = req.body as { users: User[] }
-  const added = await firestore.collection('chats').add({
-    lastActive: new Date().valueOf(),
-    users: [{
-      id: users[0].id,
-      username: users[0].username
-    }, {
-      id: users[1].id,
-      username: users[1].username
-    }]
-  })
-  // adds reference to chat in each user
-  users.forEach(user => {
-    void firestore.collection(`users/${user.username}/chats`).add({ id: added.id })
-  })
-
-  res.status(200).json({ id: added.id })
+  try {
+    const added = await firestore.collection('chats').add({})
+    res.status(200).json({ id: added.id })
+  } catch (err) {
+    res.status(500).json({ err })
+  }
 }
