d6a2b111eff41c82c7296649cfaa95bc25367040
lucadard <lucadardenne@hotmail.com>
2023-05-05 16:50:14 -0300

    add enpoint to fetch messages from a chat

---
+++ src/pages/api/messages/[chatId].ts
@@ -0,0 +1,28 @@
+import { firestore } from '@/firebase/admin'
+import { Message } from '@/types/types'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let { chatId, limit, offset } = req.query as unknown as { chatId: string, limit?: number, offset?: number }
+  limit = limit !== undefined ? +limit : 20
+  offset = offset !== undefined ? +offset : 0
+
+  console.log(offset)
+  const messages: Message[] = []
+  try {
+    const messagesRef = firestore
+      .collection(`chats/${chatId}/messages`)
+      .orderBy('createdAt', 'desc')
+      .offset(offset)
+      .limit(limit)
+    const snapshot = await messagesRef.get()
+    snapshot.forEach(doc => {
+      messages.push(doc.data() as Message)
+    })
+
+    res.status(200).json({ messages: messages.reverse() })
+  } catch (err: any) {
+    res.status(500).json({ err: err.message })
+  }
+}
