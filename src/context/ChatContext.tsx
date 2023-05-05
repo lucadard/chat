ee4d485bf6af589ff0e8d10d714db5db54a6d3b2
lucadard <lucadardenne@hotmail.com>
2023-05-05 13:03:55 -0300

    modify returned objects & functions

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -1,60 +1,38 @@
 import { useChats } from '@/hooks/useChats'
 import { useMessages } from '@/hooks/useMessages'
-import { type Chat, ChatsMap, User, Message } from '@/types/types'
+import { filterOutFromObj } from '@/lib'
+import { type Chat, ChatsMap, Message } from '@/types/types'
 import { type Session } from 'next-auth'
 import { useRouter } from 'next/router'
-import { createContext, useContext, ReactNode, useState, useEffect } from 'react'
+import { createContext, useContext, ReactNode, useState } from 'react'
 
 type State = {
-  currentChatData: {
-    lastActive?: number
-    id: string
-    name: string | undefined
-    user?: User
-  }
+  currentChat: Chat | undefined
   chats: ChatsMap
   messages: Message[]
-  newChat: Chat | undefined
-  setNewChat: (chat: Chat) => void
-  removeNewChat: () => void
   session?: Session
+  setClientChat: (id: string, chat: Chat) => void
+  removeClientChat: (newId: string) => void
 }
 type ChatProviderProps = { session: Session, children: ReactNode }
 
 const ChatStateContext = createContext<State | undefined>(undefined)
 
 function ChatProvider ({ session, children }: ChatProviderProps) {
-  const [newChat, setNewChat] = useState<Chat>()
-  const [selectedChat, setSelectedChat] = useState<keyof ChatsMap>('general')
-
+  const [clientChats, setClientChats] = useState<ChatsMap>({})
   const router = useRouter()
-  const chatId = router.query.chatId as string
-
-  const chats = useChats(session?.user)
-  const currentChat = chats[selectedChat]
-  const currentUserData = currentChat?.users.filter((user) => user.username !== session?.user?.name)[0] ?? newChat?.users[0]
-
-  const messages = useMessages(selectedChat !== 'new' ? chatId ?? 'general' : 'none')
-
-  useEffect(() => { setSelectedChat(chatId) }, [chatId])
+  const chats: ChatsMap = { ...clientChats, ...useChats(session?.user) }
+  const receiverId = router.query.chatId as keyof typeof chats
+  const currentChat = chats[receiverId] as Chat
+  const messages = useMessages(currentChat?.chat_id ?? 'general')
 
   const value = {
-    currentChatData: {
-      id: selectedChat === 'new' ? 'new' : chatId,
-      lastActive: currentChat?.lastActive,
-      user: currentUserData,
-      name: selectedChat === 'new' ? newChat?.users[0].username : currentUserData?.username ?? 'general'
-    },
-    newChat,
+    currentChat,
     chats,
     messages,
-    removeNewChat: () => { setNewChat(undefined) },
-    setNewChat: (chat: Chat) => {
-      setNewChat(chat)
-      void router.push('/chats/new', undefined, { shallow: true })
-      setSelectedChat('new')
-    },
-    session
+    session,
+    setClientChat: (id: string, chat: Chat) => { setClientChats(prev => ({ ...prev, [id]: chat })) },
+    removeClientChat: (newId: string) => { setClientChats(prev => filterOutFromObj(prev, [newId])) }
   }
   return (
     <ChatStateContext.Provider value={value}>
