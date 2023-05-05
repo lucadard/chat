6ff989a74f0fbb1cbae252cea5a1d67437197463
lucadard <lucadardenne@hotmail.com>
2023-05-05 12:53:47 -0300

    add function

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,11 @@
 export const getAvatarById = (id: string, px = '40') =>
     `https://avatars.githubusercontent.com/u/${id}?s=${px}&v=4`
+
+export const filterOutFromObj = (oldObj: any, props: string[]) => {
+  return Object.keys(oldObj)
+    .filter(key => !props.includes(key))
+    .reduce((newObj: any, key) => {
+      newObj[key] = oldObj[key]
+      return newObj
+    }, {})
+}
