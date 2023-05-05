03332753b4d343196f4a86152ee1e61711632fc9
lucadard <lucadardenne@hotmail.com>
2023-05-05 16:51:57 -0300

    changes in how listens for messages & add fn to fetch more

--- src/hooks/useMessages.tsx
+++ src/hooks/useMessages.tsx
@@ -1,32 +1,52 @@
 import { getDb } from '@/firebase/firebase'
 import { Message } from '@/types/types'
-import { collection, limit, onSnapshot, orderBy, query } from '@firebase/firestore'
+import { Unsubscribe, collection, limit, onSnapshot, orderBy, query } from '@firebase/firestore'
+import axios from 'axios'
 import { useEffect, useState } from 'react'
+
+const fetchLimit = 20
 /**
 * Retrieves messages from the given chatId and
 * keeps listening for new changes
 */
-export const useMessages = (chatId: string): Message[] => {
+export const useMessages = (chatId: string): { messages: Message[], getMoreMessages: () => void, isMoreMessages: boolean } => {
   const [messages, setMessages] = useState<Message[]>([])
+  const [isMoreMessages, setIsMoreMessages] = useState(true)
   const db = getDb()
 
   useEffect(() => {
-    if (!chatId || chatId === 'none') return setMessages([])
-    const q = query(
-      collection(db, `chats/${chatId}/messages`),
-      orderBy('createdAt'),
-      limit(50)
-    )
+    if (!chatId || chatId === 'none') return
+    setIsMoreMessages(true)
+    let unsubscribe: Unsubscribe = () => {}
+    const retrievedTime = new Date().valueOf()
 
-    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
-      const messages: any[] = []
-      QuerySnapshot.forEach((doc: { data: () => any, id: any }) => {
-        messages.push({ ...doc.data(), id: doc.id })
+    axios.get(`/api/messages/${chatId}?limit=${fetchLimit}`).then(
+      ({ data }) => setMessages(data.messages)
+    ).finally(() => {
+      const q = query(
+        collection(db, `chats/${chatId}/messages`),
+        orderBy('createdAt', 'desc'),
+        limit(1)
+      )
+      unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+        const messages: any[] = []
+        QuerySnapshot.forEach((doc: { data: () => any, id: any }) => {
+          if (doc.data().createdAt < retrievedTime) return
+          messages.push({ ...doc.data(), id: doc.id })
+        })
+        setMessages(prev => [...prev, ...messages])
       })
-      setMessages(messages)
-    })
-    return () => unsubscribe()
+    }).catch(console.error)
+    return () => { unsubscribe(); setMessages([]) }
   }, [chatId])
 
-  return messages
+  function getMoreMessages () {
+    axios.get(`/api/messages/${chatId}?offset=${messages.length}&limit=${fetchLimit}`)
+      .then(({ data }) => {
+        if (data.messages.length)setMessages(prev => ([...data.messages, ...prev]))
+        else setIsMoreMessages(false)
+      })
+      .catch(console.error)
+  }
+  return { messages, getMoreMessages, isMoreMessages }
 }
