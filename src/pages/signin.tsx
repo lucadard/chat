f70b282a47353ff05aa90ae322b4e04a986c256e
lucadard <lucadardenne@hotmail.com>
2023-05-10 13:07:34 -0300

    change page styles

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,13 +1,15 @@
 import { Provider } from 'next-auth/providers'
 import { getProviders, signIn, getSession, getCsrfToken } from 'next-auth/react'
 import { GetServerSideProps } from 'next/types'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const getServerSideProps: GetServerSideProps | {} = async (ctx) => {
   const { req } = ctx
   const session = await getSession({ req })
   if (session) {
     return {
-      redirect: { destination: '/' }
+      redirect: { destination: '/chats/general' }
     }
   }
 
@@ -21,23 +23,23 @@ export const getServerSideProps: GetServerSideProps | {} = async (ctx) => {
 
 function SignInPage ({ providers }: { providers: Provider[] }) {
   return (
-    <div>
-      {Object.values(providers).map((provider) => {
-        return (
-          <div
+    <div className='-mt-10 grid h-screen place-content-center'>
+      <div className='flex w-full max-w-md flex-col items-center rounded-md bg-secondary p-14 py-10 shadow-lg'>
+        {Object.values(providers).map((provider) =>
+          <button
             key={provider.name}
-            className='flex h-screen flex-col items-center gap-5 bg-white pt-[30vh] text-black'
+            className='flex items-center gap-3 rounded-md bg-links p-4 px-8 text-lg font-medium duration-200 hover:bg-links/50'
+            onClick={async () => await signIn(provider.id, { callbackUrl: '/chats/general' })}
           >
-            <h1 className='mb-2 text-center text-3xl font-semibold'>Welcome</h1>
-            <button
-              className='rounded-sm border-2 p-4 px-8 duration-200 hover:bg-gray-100/50'
-              onClick={async () => await signIn(provider.id, { callbackUrl: '/chats/general' })}
-            >
-              Sign in with {provider.name}
-            </button>
-          </div>
-        )
-      })}
+            Sign in with {provider.name}
+            <Image
+              src='/github-logo.png' alt='logo of Github'
+              height={30} width={30}
+            />
+          </button>
+        )}
+        <p className='mt-4'>or instead <Link href='/' className='text-start text-links duration-200 hover:underline'>go back</Link></p>
+      </div>
     </div>
   )
 }
